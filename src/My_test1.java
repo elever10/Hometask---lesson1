0905ae3de82284f1a172666b97099dfd31652c53
Elena Verkholaz <elena.verkholaz@gmail.com>
2016-10-27 18:18:54 +0300

    First commit

---
+++ src/My_test1.java
@@ -0,0 +1,10 @@
+/**
+ * Created by elve on 10/27/2016.
+ */
+public class My_test1 {
+    public static void main(String[] args) {
+        System.out.println("First text");
+        System.out.println("Second test");
+    }
+
+}
