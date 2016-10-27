91b937da5d3f6bf7aae04c26ffd89ab57395094e
Elena Verkholaz <elena.verkholaz@gmail.com>
2016-10-27 18:23:31 +0300

    Second commit

---
+++ src/My_test2.java
@@ -0,0 +1,8 @@
+/**
+ * Created by elve on 10/27/2016.
+ */
+public class My_test2 {
+    public static void main(String[] args) {
+        System.out.println("for second method - text1");
+    }
+}
